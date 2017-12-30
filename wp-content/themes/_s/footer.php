5cdb79c724c856803565aafe4085546300ab6d80
silviafuen <silvia.fuen@icloud.com>
2017-12-30 17:42:44 -0600

    New Styling + Structure
    Implemented new styling elements to make it more responsive and
    modified the structure to have a header, body, and footer section all
    separate for future solo modifying

--- wp-content/themes/_s/footer.php
+++ wp-content/themes/_s/footer.php
@@ -11,22 +11,27 @@
 
 ?>
 
-	</div><!-- #content -->
+			</div><!-- #content -->
+		</div><!-- #site-content -->
+	</div><!-- #main-content-area -->
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'trial-theme' ) ); ?>"><?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'trial-theme' ), 'WordPress' );
-			?></a>
-			<span class="sep"> | </span>
-			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'trial-theme' ), 'trial-theme', '<a href="http://silviafuentes.me">Silvia Fuentes</a>' );
-			?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-
+	<div class="footer-area full">
+		<div class="main-page">
+			<footer id="colophon" class="site-footer inner">
+				<div class="site-info">
+					<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'trial-theme' ) ); ?>"><?php
+						/* translators: %s: CMS name, i.e. WordPress. */
+						printf( esc_html__( 'Proudly powered by %s', 'trial-theme' ), 'WordPress' );
+					?></a>
+					<span class="sep"> | </span>
+					<?php
+						/* translators: 1: Theme name, 2: Theme author. */
+						printf( esc_html__( 'Theme: %1$s by %2$s.', 'trial-theme' ), 'trial-theme', '<a href="http://silviafuentes.me">Silvia Fuentes</a>' );
+					?>
+				</div><!-- .site-info -->
+			</footer><!-- #colophon -->
+		</div><!-- #main-page -->
+	</div><!-- #footer-area -->
 
 <?php wp_footer(); ?>
 
